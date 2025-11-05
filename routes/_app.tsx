5ba040855d3d75db0dc98a0b90936f1898591972
Timi <jjtimi888@gmail.com>
2025-11-05 13:16:55 +0700

    Update _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,7 +8,6 @@ export default define.page(function App({ Component, state }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Deno fresh example" />
         <title>{state.title ?? "with-fresh"}</title>
-        <link rel="preload" href="/style.css" as="style" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
